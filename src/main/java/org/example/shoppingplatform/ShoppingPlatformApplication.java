6e31916b3a41de689a60c9f591de61fefb834ee6
JiajunJasonLi <jiajunlishenzhong@outlook.com>
2026-07-25 15:11:48 -0400

    Initial commit

---
+++ src/main/java/org/example/shoppingplatform/ShoppingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoppingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shoppingplatform/ShoppingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
